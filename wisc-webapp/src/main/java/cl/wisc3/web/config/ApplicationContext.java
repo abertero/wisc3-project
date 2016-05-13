52bb8f328de3c3bdadef91d200efa1a5ef3e318a
Aldo Bertero <thescientist14@gmail.com>
2016-05-13 10:46:01 -0300

    [wisc-webapp] Adds JPA persistence configuration

--- wisc-webapp/src/main/java/cl/wisc3/web/config/ApplicationContext.java
+++ wisc-webapp/src/main/java/cl/wisc3/web/config/ApplicationContext.java
@@ -1,11 +1,15 @@
 package cl.wisc3.web.config;
 
+import com.jolbox.bonecp.BoneCPDataSource;
+import org.hibernate.ejb.HibernatePersistence;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.*;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.env.Environment;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -13,6 +17,9 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Properties;
 
 @Configuration
 @ComponentScan(basePackages = {"cl.wisc3.web"})
@@ -22,6 +29,19 @@ import javax.annotation.Resource;
 @PropertySource("classpath:config/application.properties")
 public class ApplicationContext {
     private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationContext.class);
+
+    private static final String PROPERTY_NAME_DATABASE_DRIVER = "db.driver";
+    private static final String PROPERTY_NAME_DATABASE_PASSWORD = "db.password";
+    private static final String PROPERTY_NAME_DATABASE_URL = "db.url";
+    private static final String PROPERTY_NAME_DATABASE_USERNAME = "db.username";
+
+    private static final String PROPERTY_NAME_HIBERNATE_DIALECT = "hibernate.dialect";
+    private static final String PROPERTY_NAME_HIBERNATE_FORMAT_SQL = "hibernate.format_sql";
+    private static final String PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY = "hibernate.ejb.naming_strategy";
+    private static final String PROPERTY_NAME_HIBERNATE_SHOW_SQL = "hibernate.show_sql";
+    private static final String PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO = "hibernate.hbm2ddl.auto";
+    private static final String PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN = "entitymanager.packages.to.scan";
+
     private static final String PROPERTY_NAME_MESSAGESOURCE_BASENAME = "message.source.basename";
     private static final String PROPERTY_NAME_MESSAGESOURCE_USE_CODE_AS_DEFAULT_MESSAGE = "message.source.use.code.as.default.message";
 
@@ -30,6 +50,57 @@ public class ApplicationContext {
 
     @Resource
     private Environment environment;
+
+    @Bean
+    public DataSource dataSource() {
+        BoneCPDataSource dataSource = new BoneCPDataSource();
+
+        dataSource.setDriverClass(environment.getRequiredProperty(PROPERTY_NAME_DATABASE_DRIVER));
+        dataSource.setJdbcUrl(environment.getRequiredProperty(PROPERTY_NAME_DATABASE_URL));
+        dataSource.setUsername(environment.getRequiredProperty(PROPERTY_NAME_DATABASE_USERNAME));
+        dataSource.setPassword(environment.getRequiredProperty(PROPERTY_NAME_DATABASE_PASSWORD));
+
+        try {
+            String catalog = dataSource.getConnection().getCatalog();
+            LOGGER.warn(String.format("EXITO CONECTANDOSE A DB %s", catalog));
+        } catch (SQLException e) {
+            LOGGER.error(String.format("NO SE PUDO CONNECTAR A LA DB %s", PROPERTY_NAME_DATABASE_URL), e);
+        }
+
+        LOGGER.info(String.format("Init dataSource %s", dataSource));
+        return dataSource;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager() throws ClassNotFoundException {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+
+        transactionManager.setEntityManagerFactory(entityManagerFactoryBean().getObject());
+
+        LOGGER.info(String.format("Init transactionManager %s", transactionManager));
+        return transactionManager;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() throws ClassNotFoundException {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setPackagesToScan(environment.getRequiredProperty(PROPERTY_NAME_ENTITYMANAGER_PACKAGES_TO_SCAN));
+        entityManagerFactoryBean.setPersistenceProviderClass(HibernatePersistence.class);
+
+        Properties jpaProterties = new Properties();
+        jpaProterties.put(PROPERTY_NAME_HIBERNATE_DIALECT, environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_DIALECT));
+        jpaProterties.put(PROPERTY_NAME_HIBERNATE_FORMAT_SQL, environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_FORMAT_SQL));
+        jpaProterties.put(PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY, environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_NAMING_STRATEGY));
+        jpaProterties.put(PROPERTY_NAME_HIBERNATE_SHOW_SQL, environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_SHOW_SQL));
+        jpaProterties.put(PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO, environment.getRequiredProperty(PROPERTY_NAME_HIBERNATE_HBM2DDL_AUTO));
+
+        entityManagerFactoryBean.setJpaProperties(jpaProterties);
+
+        LOGGER.info(String.format("Init entityManagerFactory %s", entityManagerFactoryBean));
+        return entityManagerFactoryBean;
+    }
 
     @Bean
     public MessageSource messageSource() {
@@ -50,7 +121,7 @@ public class ApplicationContext {
         viewResolver.setPrefix(VIEW_RESOLVER_PREFIX);
         viewResolver.setSuffix(VIEW_RESOLVER_SUFFIX);
 
-        LOGGER.info("Init viewResolver " + viewResolver.toString());
+        LOGGER.info(String.format("Init viewResolver %s", viewResolver));
         return viewResolver;
     }
 }
